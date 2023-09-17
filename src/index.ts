7afb8d8aef161138e58c51fddcec503c4e946e31
Johnny Guye <johnnyyuge@gmail.com>
2023-09-17 17:32:03 +0200

    chore: bump to v0.3.0

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from "./firestorm"
 export * from "./repository"
 export * from "./query"
 export * from "./decorators"
+export * from "./core/firestorm-model"
 
 export { firestormMetadatasToJson } from "./storage"
